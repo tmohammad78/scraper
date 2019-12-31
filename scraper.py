cf33f5499fc2db2dbd994246b2a3a5f0263005d4
mohammad <mohammadtaheri506@gmail.com>
2019-12-31 23:06:13 +0330

    fix

--- scraper.py
+++ scraper.py
@@ -1,24 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
+res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 
-res=requests.get('https://news.ycombinator.com/newest')
-# print(res.text)
-soup = BeautifulSoup(res.text,'html.parser')
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
-# print(link)
+links2 = soup2.select('.storylink')
+subtext2 = soup2.select('.subtext')
 
-def create_custom(links,subtext):
-    hn=[]
-    for idx,item in enumerate(links):
-        title = item.getText()
-        href = item.get('href', None)
-        vote = subtext[idx].select('.score')
-        if len(vote) :
-            points = int(vote[0].getText().replace(' points', ''))
-            hn.append({'title':title , 'link':href , 'votes':points})
-    return hn
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
+def create_custom_hn(links, subtext):
+  hn = []
+  for idx, item in enumerate(links):
+    title = item.getText()
+    href = item.get('href', None)
+    vote = subtext[idx].select('.score')
+    if len(vote):
+      points = int(vote[0].getText().replace(' points', ''))
 
-print(create_custom(links,subtext))
+      if points > 99:
+        hn.append({'title': title, 'link': href, 'votes': points})
+  return hn
+ 
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
